b65e42304cc493472899c8d64d21cba0aa8d2c28
Erik <erik_97@live.nl>
2023-04-19 16:00:07 +0200

    Add execute for create command

--- commands/commands.go
+++ commands/commands.go
@@ -1,11 +1,25 @@
 package pcsdk
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"net/netip"
 
 	"github.com/google/uuid"
 	"github.com/thefeli73/polemos/state"
 )
+
+type ExecuteCommand interface {
+	Execute(string) error
+}
+
+type response struct {
+	message string `json:"message"`
+}
 
 type ProxyCommandCreate struct {
 	Command CommandCreate `json:"create"`
@@ -17,6 +31,36 @@ type CommandCreate struct {
 	DestinationPort uint16     `json:"destination_port"`
 	DestinationIP   netip.Addr `json:"destination_ip"`
 	Id              string     `json:"id"`
+}
+
+func (c ProxyCommandCreate) Execute(url string) error {
+	data, err := json.Marshal(c)
+	if err != nil {
+		return errors.New(fmt.Sprintf("could not serialize: %s\n", err))
+	}
+
+	requestURL := fmt.Sprintf("%s/command", url)
+
+	bodyReader := bytes.NewReader(data)
+
+	res, err := http.DefaultClient.Post(requestURL, "application/json", bodyReader)
+	if err != nil {
+		return errors.New(fmt.Sprintf("error making http request: %s\n", err))
+	}
+
+	fmt.Println(res)
+
+	body, err := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
+	if err != nil {
+		return errors.New(fmt.Sprintf("error reading response: %s\n", err))
+	}
+
+	if res.StatusCode != 202 {
+		return errors.New(fmt.Sprintf("error processing command: (%d) %s\n", res.StatusCode, body))
+	} else {
+		return nil
+	}
 }
 
 func NewCommandCreate(iport uint16, oport uint16, oip netip.Addr, id state.CustomUUID) ProxyCommandCreate {
